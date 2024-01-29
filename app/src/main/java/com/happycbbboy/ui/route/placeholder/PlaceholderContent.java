66f71856f1c7d4229427e2de1c6e7c0cc715dc36
happycbbboy <happycbbboy@gmail.com>
2024-01-29 19:23:39 +0800

    列表展示优化

--- app/src/main/java/com/happycbbboy/ui/route/placeholder/PlaceholderContent.java
+++ app/src/main/java/com/happycbbboy/ui/route/placeholder/PlaceholderContent.java
@@ -7,12 +7,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- * <p>
- * TODO: Replace all uses of this class before publishing your app.
- */
 public class PlaceholderContent {
 
     /**
