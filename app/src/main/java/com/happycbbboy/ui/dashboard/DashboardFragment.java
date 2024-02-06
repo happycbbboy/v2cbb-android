69745c7d9f7df352fadcbffdb6dc962d393a7b93
happycbbboy <happycbbboy@gmail.com>
2024-02-06 19:20:11 +0800

    v1 over

--- app/src/main/java/com/happycbbboy/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/happycbbboy/ui/dashboard/DashboardFragment.java
@@ -5,29 +5,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
 import com.happycbbboy.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-private FragmentDashboardBinding binding;
+    private FragmentDashboardBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
+                             ViewGroup container, Bundle savedInstanceState) {
         DashboardViewModel dashboardViewModel =
                 new ViewModelProvider(this).get(DashboardViewModel.class);
 
-    binding = FragmentDashboardBinding.inflate(inflater, container, false);
-    View root = binding.getRoot();
+        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
         final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView.setText(
+                "如果您想支持我可以通过 buy me a coffee捐助我\n"+
+                "buymeacoffee是一个将开发人员、设计师、艺术家等与其社区联系起来的网站，以便人们表达对他们工作的赞赏。\n" +
+                        "我的捐助地址是:\n" +
+                        "https://www.buymeacoffee.com/happycbbboy\n"
+        );
         return root;
     }
 
-@Override
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
--- app/src/main/java/com/happycbbboy/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/happycbbboy/ui/slideshow/SlideshowFragment.java
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.happycbbboy.databinding.FragmentSlideshowBinding;
@@ -26,7 +25,14 @@ public class SlideshowFragment extends Fragment {
         View root = binding.getRoot();
 
         final TextView textView = binding.textSlideshow;
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        textView.setText("您可以通过 happycbbboy@gmail.com 邮箱和我们联系\n" +
+                "我们能提供什么\n" +
+                "1.vpn标准sdk\n" +
+                "2.vpn定制化sdk\n" +
+                "3.vpn实现教程\n" +
+                "4.如果您有任何其他想法均可通过该邮箱和我们联系\n"
+        );
         return root;
     }
 
